1056d26e5dd42244f736f90d79152e2dd48f2851
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-03 19:12:06 +0000

    removed duplicated api call

--- src/App.tsx
+++ src/App.tsx
@@ -29,21 +29,21 @@ ChartJS.register(
 
 function App() {
   const [cryptos, setCryptos] =useState<Crypto[] | null>(null);
-  const [selected, setSelected] =useState <Crypto | null >();
-  const [data, setData] = useState<ChartData<"line">>();
-  const [range, setRange] =useState<Number>(30);
-  const [options,setOptions] = useState<ChartOptions<"line">>({
-    responsive: true,
-    plugins: {
-      legend: {
-        display:false,
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
-  });
+  const [selected, setSelected] =useState <Crypto [] >([]);
+  // const [data, setData] = useState<ChartData<"line">>();
+  // const [range, setRange] =useState<Number>(30);
+  // const [options,setOptions] = useState<ChartOptions<"line">>({
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       display:false,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: 'Chart.js Line Chart',
+  //     },
+  //   },
+  // });
 
 
   useEffect(() => {
@@ -53,68 +53,50 @@ function App() {
     });
   },[]);
 
-  useEffect(()=>{
-    if (!selected) return;
-       //request 
-       axios.get(`https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${range === 1 ? 'interval=hourly' :`interval=daily`}`)
-       .then((response) => {
-        //setdata
+  // useEffect(()=>{
+  //   if (!selected) return;
+  //      //request 
+  //      axios.get(`https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${range === 1 ? 'interval=hourly' :`interval=daily`}`)
+  //      .then((response) => {
+  //       //setdata
 
-        console.log(response.data);
-        setData({
-            labels:response.data.prices.map((price: number[]) =>{
-              return moment.unix(price[0]/ 1000).format(range === 1 ? 'HH:MM': 'DD-MM-YYYY');
+  //       console.log(response.data);
+  //       setData({
+  //           labels:response.data.prices.map((price: number[]) =>{
+  //             return moment.unix(price[0]/ 1000).format(range === 1 ? 'HH:MM': 'DD-MM-YYYY');
 
-            }),
-            datasets: [
-              {
-                label: 'Dataset 1',
-                data: response.data.prices.map((price:number[]) =>{return price[1]}),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-              },
-            ],
-        });
-        setOptions({responsive: true,
-          plugins: {
-            legend: {
-             display:false,
-            },
-            title: {
-              display: true,
-              text: ` ${selected?.name} Price over last ` + range + (range === 1 ? 'Day.':  'Days(s).'),
-            },
-          },})
-       });
-       // update data state
+  //           }),
+  //           datasets: [
+  //             {
+  //               label: 'Dataset 1',
+  //               data: response.data.prices.map((price:number[]) =>{return price[1]}),
+  //               borderColor: 'rgb(255, 99, 132)',
+  //               backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  //             },
+  //           ],
+  //       });
+  //       setOptions({responsive: true,
+  //         plugins: {
+  //           legend: {
+  //            display:false,
+  //           },
+  //           title: {
+  //             display: true,
+  //             text: ` ${selected?.name} Price over last ` + range + (range === 1 ? 'Day.':  'Days(s).'),
+  //           },
+  //         },})
+  //      });
+  //      // update data state
     
-  }, [selected,range]);
+  // }, [selected,range]);
   return (
     <>
     <div className="App">
       <select onChange={(e) =>{
-       const c =  cryptos?.find((x) => x.id === e.target.value);
-       setSelected(c);
+       const c =  cryptos?.find((x) => x.id === e.target.value) as Crypto;
+       setSelected([...selected,c]);
        //request 
-       axios.get(`https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`).then((response) => {
-        //setdata
 
-        console.log(response.data)
-        setData({
-            labels:response.data.prices.map((price: number[]) =>{
-              return moment.unix(price[0]/ 1000).format('DD-MM-YYYY');
-
-            }),
-            datasets: [
-              {
-                label: 'Dataset 1',
-                data: response.data.prices.map((price:number[]) =>{return price[1]}),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-              },
-            ],
-        })
-       })
        // update data state
        }}
        defaultValue="default"
@@ -125,16 +107,13 @@ function App() {
       }) : <p>no cryptos !!</p>}
       <option value="default">choose an option</option>
       </select>
-      <select onChange={(e)=>{
-        setRange(parseInt(e.target.value));
-      }}>
-        <option value={30}>30 days</option>
-        <option value={7}>7 days</option>
-        <option value={1}>1 day</option>
-      </select>
     </div>
-    {selected &&<CryptoSummary crypto={selected}  />}
-    {data &&<div style={{width:600}}><Line options={options} data={data}  /></div>}
+
+    {selected.map((s) => { return <CryptoSummary crypto={s}  />
+
+    })}
+    {/* {selected &&<CryptoSummary crypto={selected}  />} */}
+    {/* {data &&<div style={{width:600}}><Line options={options} data={data}  /></div>} */}
     </>
     
   );
