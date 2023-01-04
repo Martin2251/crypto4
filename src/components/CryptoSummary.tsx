c2f86717ade9781cf55e882d0a3b6f36a271f10f
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-04 06:45:12 +0000

    calculate values

--- src/components/CryptoSummary.tsx
+++ src/components/CryptoSummary.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react'
 import { Crypto } from '../types/Crypto'
 
 export type AppProps = {
@@ -6,11 +6,18 @@ export type AppProps = {
 }
 
 const CryptoSummary = ({crypto}:AppProps): JSX.Element => {
+
+  useEffect(() => {
+    console.log(crypto.name, amount, crypto.current_price * parseFloat(amount));
+  })
+
+  const [amount, setAmount] = useState<string>('0');
   return (
-    <>
+    <div>
    <span> {crypto.name + '$' + crypto.current_price}</span>
-   <input defaultValue={1000}></input>
-   </>
+   <input value={amount} style={{margin:10}} onChange={(e) =>{setAmount(e.target.value)}} type="number"></input>
+   <p> ${(crypto.current_price * parseFloat(amount)).toLocaleString(undefined,{minimumFractionDigits:2, maximumFractionDigits:2})}</p>
+   </div>
   )
 }
 
